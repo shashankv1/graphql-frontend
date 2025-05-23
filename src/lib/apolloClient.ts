eba46cd4e054399ed881cc043113433f957b6c02
shashankv1 <shashank@webknot.in>
2025-05-23 13:30:00 +0530

    applying deployed backend url

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -3,6 +3,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql", // Your backend server
+  uri: "https://graphql-backend-qpb4.onrender.com/graphql", // Your backend server
   cache: new InMemoryCache(),
 });
